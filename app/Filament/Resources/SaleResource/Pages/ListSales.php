95931dcec86fbe9bbaaabea3ee7bf5499e7f4ffd
lucidlynxx <dzakysyahrizal04@gmail.com>
2023-08-17 00:29:12 +0700

    implementasi fitur print excel

--- app/Filament/Resources/SaleResource/Pages/ListSales.php
+++ app/Filament/Resources/SaleResource/Pages/ListSales.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\SaleResource\Pages;
 
 use App\Filament\Resources\SaleResource;
 use Filament\Pages\Actions;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\View\View;
 
@@ -14,6 +16,15 @@ class ListSales extends ListRecords
     protected function getActions(): array
     {
         return [
+            ExportAction::make()
+                ->exports([
+                    ExcelExport::make('table')
+                        ->fromTable()
+                        ->withFilename('Laporan Penjualan tanggal ' . date('d M Y')),
+                ])
+                ->label('Excel')
+                ->color('success')
+                ->icon('heroicon-o-document-text'),
             Actions\Action::make('Pdf')
                 ->color('danger')
                 ->icon('heroicon-o-document-text')
